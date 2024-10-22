c4f61f2a48f36b44c0d0c03ed8458bf61fb49e74
wahyuwerayana <wahyuwerayana@gmail.com>
2024-10-22 21:12:49 +0700

    Update 22.10.2024
    - Menambahkan Logo Aplikasi
    - Sistem Splash Screen Selesai
    - Menambahkan beberapa bahasa pemrograman baru
    - Menambahkan Scene untuk pemilihan level

---
+++ Assets/Scripts/SplashScreen/SplashScreenManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.PlasticSCM.Editor.WebApi;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScreenManager : MonoBehaviour
+{
+    bool canSwitchScene = false;
+    public TMP_Text touchText;
+
+    public void ChangeScene(int index){
+        SceneManager.LoadScene(index);
+    }
+
+    private void Update() {
+        if(!canSwitchScene)
+            return;
+
+        if(Input.touchCount > 0){
+            Touch touch = Input.GetTouch(0);
+            if(touch.phase == TouchPhase.Began){
+                ChangeScene(1);
+            }
+        }
+            
+    }
+
+    public void ChangeBoolState(){
+        canSwitchScene = true;
+        touchText.gameObject.SetActive(true);
+    }
+}
